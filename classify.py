38cad1cfeab833ae8691d95131e125079b058ed7
gael-rktdrv <max.kzntsv@yandex.ru>
2021-04-17 11:02:25 +0200

    Commit

---
+++ classify.py
@@ -0,0 +1,174 @@
+from collections import Counter
+import pandas as pd
+import numpy as np
+from scipy.stats import t
+
+
+class TwoIndependentSamples:
+
+    def __init__(self, df, field, ref, cat):
+        self.df = df  # The DataFrame
+        self.cat = cat  # The category: categories of values in the field
+        self.field = field  # The field: the series of the dataframe which will be analyzed
+        self.ref = ref  # The reference: the series of the dataframe which will be used for analysis
+
+    def get_count(self):
+        """
+        Get the count of the two groups
+        df: the dataframe
+        cat: one of the categories in field to be investigated
+        field: one of the fields of the dataframe to be investigated
+        ref: the reference to be used for comparison.
+
+        e.g.: Investigating about pay gap per gender
+        -> field = gender
+        -> ref = salary
+        -> cat = male
+        """
+        count = Counter(self.df[self.field])
+        count_ovh = {i: j for i, j in count.items()}
+
+        count_ovh = pd.DataFrame(list(
+            count_ovh.items()), columns=[self.field, 'count']
+        )
+
+        count_mean = list(
+            map(lambda row: round(row, 2), self.df.groupby(self.field)[self.ref].mean())
+        )
+
+        count_std = list(
+            map(lambda row: round(row, 2), self.df.groupby(self.field)[self.ref].std())
+        )
+
+        count_ovh['mean'] = count_mean
+        count_ovh['std'] = count_std
+        count_ovh['var'] = count_ovh['std'].apply(lambda row: row ** 2)
+
+        return count_ovh
+
+    def compare_categories(self):
+        """
+        Compares one of the categories to the others
+        cat: is the value to be compared with the remaining categories
+
+        e.g.: Investigating about pay gap per position
+        -> field = Positions
+        -> ref = salary
+        -> cat = CEO
+        -> remaining: non-CEO
+        """
+        # The categories which will be compared with the chosen one
+        remaining = "non-" + self.cat
+        # DataFrame values
+        comp_values = [self.cat, remaining]
+
+        # DataFrame field values
+        comp_count = [
+            self.df[self.df[self.field] == self.cat][self.ref].count(),
+            self.df[self.df[self.field] != self.cat][self.ref].count()
+        ]
+        comp_mean = [
+            self.df[self.df[self.field] == self.cat][self.ref].mean(),
+            self.df[self.df[self.field] != self.cat][self.ref].mean()
+        ]
+        comp_std = [
+            self.df[self.df[self.field] == self.cat][self.ref].std(),
+            self.df[self.df[self.field] != self.cat][self.ref].std()
+        ]
+
+        # Creating the dataframe
+        comp_ovh = pd.DataFrame({
+            'Keys': comp_values,
+            'count': comp_count,
+            'mean': comp_mean,
+            'Std': comp_std
+        })
+
+        # Adding columns to the dataframe
+        comp_ovh[['mean', 'Std']] = comp_ovh[['mean', 'Std']].applymap(lambda i: round(i, 2))
+        comp_ovh['var'] = comp_ovh['Std'].apply(lambda i: i ** 2)  # Variance
+
+        return comp_ovh
+
+    @staticmethod
+    def pooled_variance(size1, size2, std1, std2):
+        """Return the pooled variance between two samples"""
+        return ((size1 - 1) * std1 ** 2 + (size2 - 1) * std2 ** 2) / (size1 + size2 - 2)
+
+    @staticmethod
+    def std_err(pooled_var, size1, size2):
+        """Returns the standard error of two samples"""
+        return np.sqrt(pooled_var / size1 + pooled_var / size2)
+
+    @staticmethod
+    def degree_of_freedom(list1, list2):
+        """Returns the degree of freedom of two samples"""
+        return len(list1) + len(list2) - 2
+
+    @staticmethod
+    def mean_diff(mean1, mean2):
+        """Returns the mean difference of two samples"""
+        return mean1 - mean2
+
+    @classmethod
+    def get_t_score(cls, m1, m2, d0, pooled_var, n1, n2):
+        """
+        Returns the T-score from all the parameters such as means of the two samples,
+        the pooled variance, the sizes of the samples
+        """
+        return (cls.mean_diff(m1, m2) - d0) / (np.sqrt(pooled_var * (1 / n1 + 1 / n2)))
+
+    @staticmethod
+    def get_p_value(t_score, dof):
+        """Returns the p-score from T-score and degree of freedom"""
+        return t.sf(abs(t_score), dof)
+
+    def get_stats_values(self, comp_ovh, d0):
+        """
+        Used for comparison of two samples
+        :return: the stats values pooled variance, p-value, T-value and compares with
+        alpha (.1, .05, .01)
+        """
+
+        pooled_var = self.pooled_variance(
+            comp_ovh['count'][0], comp_ovh['count'][1],
+            comp_ovh['Std'][0], comp_ovh['Std'][1]
+        )
+
+        t_score = self.get_t_score(
+            comp_ovh['mean'][0], comp_ovh['mean'][1], d0,
+            pooled_var, comp_ovh['count'][0], comp_ovh['count'][1]
+        )
+
+        dof = self.degree_of_freedom(
+            self.df[self.df[self.field] == self.cat][self.ref],
+            self.df[self.df[self.field] != self.cat][self.ref]
+        )
+
+        # Getting the p-value from the value of T-score
+        p_val = self.get_p_value(t_score, dof)
+
+        return t_score, p_val
+
+
+class SplitDataFrame:
+
+    def __init__(self, df, field, lim_value, direction):
+        """
+        Return a slice of the dataframe corresponding to the parameters
+        :param df: the dataframe
+        :param field: the series of the dataframe, from which it will be sliced
+        :param lim_value: the limit of the series value
+        :param direction: either higher or lower
+        """
+        self.df = df
+        self.field = field
+        self.lim_value = lim_value
+        self.direction = direction
+
+    def return_dataframe(self):
+        """Returns the corresponding dataframe"""
+        if self.direction == 'higher':
+            return self.df[self.df[self.field] > self.lim_value]
+        elif self.direction == 'lower':
+            return self.df[self.df[self.field] < self.lim_value]
---
+++ utils.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import classify
+
+
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+
+def loading_data():
+    """
+    Opening and loading data
+    path: the path to the file
+    wb_name:the name of the file (.xlsx)
+    """
+    wb_name = input("Type the filename: ")
+    gap = pd.read_excel(wb_name, sheet_name='Dataset', header=3)
+    del gap['Unnamed: 0']
+    del gap['Unnamed: 11']
+    print(f"\nDataFrame columns:")
+    for i in list(gap.columns):
+        print(f"\t{i}")
+
+    return gap
+
+
+def slicing_dataframe():
+    """Choosing to slice or not the data frame"""
+    print("\nChoose from the list:")
+    print("\t1-work with the entire dataframe.")
+    print("\t2-divide the dataframe.\n")
+    temp = int(input("Your choice 1 or 2: "))
+    limit, column = 0, None
+    if temp == 1:
+        direction = 'entire'
+        return direction, limit, column
+    elif temp == 2:
+        print("\nChoose from the list.")
+        print("Be aware that you can only choose from the list above and only numerical")
+        print("and choose either higher or lower\n")
+        column = input("Type the column (among the columns): ")
+        limit = int(input("Type the value limit (number): "))
+        print("\n\t1-choose value higher than the limit.")
+        print("\t2-choose value lower than the limit.\n")
+        temp = int(input("Your choice 1 or 2: "))
+        if temp == 1:
+            direction = 'higher'
+            return direction, limit, column
+        elif temp == 2:
+            direction = 'lower'
+            return direction, limit, column
+
+
+def parameters_input(df):
+    """Inputting the data"""
+    print("\nWorking on the dataframe")
+    field = input("\nType the field: ")
+    ref = input("\nType the reference: ")
+    print(f"\nThe values in {field}:")
+    for i in set(df[field]):
+        print(f"\t{i}")
+
+    cat = input("\nType the category of reference: ")
+    return df, field, ref, cat
+
+
+def get_hypothesis():
+    """Get the Null hypothesis"""
+    print("\nThis is the main step of the work, we have to formulate the Null hypothesis.")
+    print("Then we return it as a value to be checked by our algorithm\n")
+    print("--------------------------------EXAMPLES----------------------------------------")
+    print("Example 1: we want to check if apples in NY are more expensive than apples in LA.\n")
+    print("The null hypothesis will be:")
+    print("H0: $\mu_{NY} - \mu_{LA} <= 0$")
+    print("H1: $\mu_{NY} - \mu_{LA} > 0$\n")
+    print("Example 2: we want to check if the grades of eng students are 4% lower than mngmt students.")
+    print("The null hypothesis will be:")
+    print("H0: $\mu_{eng} - \mu_{mngmt} <= .04$")
+    print("H1: $\mu_{eng} - \mu_{mngmt} > .04$\n")
+    print("Then the value <0> or <.04> will be the reference value.")
+    print("--------------------------------------------------------------------------------\n")
+    d0 = float(input("Type the reference value: "))
+
+    return d0
+
+
+def main_ops():
+    """Loading all the attributes"""
+    df = loading_data()
+    direction, limit, column = slicing_dataframe()  # Slicing of not
+    df, field, ref, cat = parameters_input(df)  # Loading data
+
+    if direction == "entire":
+        pass
+    else:
+        df = classify.SplitDataFrame(df, column, limit, direction)
+        df = df.return_dataframe()
+
+    print(type(df))
+
+    res = classify.TwoIndependentSamples(df, field, ref, cat)  # Assigning them to the calls Classify
+    print(f"\nSummary of {field}: \n{res.get_count()}")  # Data Summary
+    d0 = get_hypothesis()  # Get the null hypothesis value
+    overall = res.compare_categories()
+    print(f"\nComparison of {field}: \n{overall}")  # Result of the comparison
+    t_score, p_val = res.get_stats_values(overall, d0)
+    print(f"\nFor {field}: \nT-score: {t_score:.3f}\np-value: {p_val:.3f}")  # T-score and p-value
